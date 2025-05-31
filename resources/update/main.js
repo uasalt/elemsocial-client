36533235fec9eb5e3a0b04a69b08f4dc34e9c6d0
Соль <147088527+uasalt@users.noreply.github.com>
2025-05-31 17:38:05 +0300

    Update main.js

--- resources/update/main.js
+++ resources/update/main.js
@@ -1,55 +1,26 @@
-const { app, BrowserWindow, ipcMain } = require('electron')
-const fetch = require('node-fetch')
-const fs = require('fs')
-
-let mainWindow
-
 const autoupdate = async () => {
-    var stats = fs.statSync(app.getAppPath())
+    var stats = await fsPromises.stat(app.getAppPath())
     if (stats.isFile()) { return }
-    async function scan(scanpath='') {
-        fetch("https://api.github.com/repositories/938380151/contents/resources/update" + scanpath)
-            .then(r => {
-                return r.json()
-            })
-            .then(async r => {
-                await r.forEach(async path => {
-                    if (path.type == 'dir') {
-                        fs.mkdir(`${app.getAppPath()}\\${scanpath}\\${path.name}`)
-                        await scan(`/${scanpath}/${path.name}`)
-                    } else if (path.type == 'file') {
-                        await fetch(`https://raw.githubusercontent.com/uasalt/elemsocial-client/refs/heads/main/resources/update/${scanpath}/${path.name}`)
-                            .then(r => {
-                                return r.buffer()
-                            })
-                            .then(r => {
-                                fs.writeFileSync(`${app.getAppPath()}/${scanpath}/${path.name}`, r)
-                            })
-                    }
-                })
-            })
-    }
-    scan()
-}
+    async function scan(scanpath = '') {
+        const url = `https://api.github.com/repositories/938380151/contents/resources/update${scanpath}`
+        const response = await fetch(url)
+        if (response.status === 404) return
 
-app.whenReady().then( async () => {
-    await autoupdate()
-    mainWindow = new BrowserWindow({
-        width: 1100,
-        height: 600,
-        minWidth: 536,
-        minHeight: 600,
-        icon: "resources/assets/images/logo.png",
-        backgroundColor: "#000000",
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false
+        const r = await response.json()
+
+        for (const pathItem of r) {
+            const localPath = pathModule.join(app.getAppPath(), scanpath, pathItem.name)
+
+            if (pathItem.type === 'dir') {
+                await fsPromises.mkdir(localPath, { recursive: true })
+                await scan(`${scanpath}/${pathItem.name}`)
+            } else if (pathItem.type === 'file') {
+                console.log(pathItem.name)
+                const fileResponse = await fetch(`https://raw.githubusercontent.com/uasalt/elemsocial-client/refs/heads/main/resources/update${scanpath}/${pathItem.name}`)
+                const buffer = Buffer.from(await fileResponse.arrayBuffer())
+                await fsPromises.writeFile(localPath, buffer)
+            }
         }
-    });
-    mainWindow.setMenu(null)
-    ipcMain.on('devtools', () => {
-        mainWindow.webContents.openDevTools()
-    })
-
-    mainWindow.loadFile('resources/auth.html')
-})
+    }
+    await scan()
+}
