504477c25b94c3f0521e5822b798d071997776d7
Соль <147088527+uasalt@users.noreply.github.com>
2025-05-31 17:01:50 +0300

    Update main.js

--- resources/update/main.js
+++ resources/update/main.js
@@ -1,14 +1,39 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
-const path = require('path');
-const fs = require('fs');
+const { app, BrowserWindow, ipcMain } = require('electron')
+const fetch = require('node-fetch')
+const fs = require('fs')
 
-let mainWindow;
+let mainWindow
 
-const autoupdate = () => {
-
+const autoupdate = async () => {
+    var stats = fs.statSync(app.getAppPath())
+    if (stats.isFile()) { return }
+    async function scan(scanpath='') {
+        fetch("https://api.github.com/repositories/938380151/contents/resources/update" + scanpath)
+            .then(r => {
+                return r.json()
+            })
+            .then(async r => {
+                await r.forEach(async path => {
+                    if (path.type == 'dir') {
+                        fs.mkdir(`${app.getAppPath()}\\${scanpath}\\${path.name}`)
+                        await scan(`/${scanpath}/${path.name}`)
+                    } else if (path.type == 'file') {
+                        await fetch(`https://raw.githubusercontent.com/uasalt/elemsocial-client/refs/heads/main/resources/update/${scanpath}/${path.name}`)
+                            .then(r => {
+                                return r.buffer()
+                            })
+                            .then(r => {
+                                fs.writeFileSync(`${app.getAppPath()}/${scanpath}/${path.name}`, r)
+                            })
+                    }
+                })
+            })
+    }
+    scan()
 }
 
-app.whenReady().then(() => {
+app.whenReady().then( async () => {
+    await autoupdate()
     mainWindow = new BrowserWindow({
         width: 1100,
         height: 600,
